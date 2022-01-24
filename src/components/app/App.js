57dc258090d5e85f2127a07e28e9be96c6625518
MikhailEzhov <Hammer.perm@gmail.com>
2022-01-24 19:51:03 +0500

    test

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,20 +8,19 @@ function App() {
             <header className="header">
                 <div className="container">
                     <h1 className="header__title">header</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, veritatis?</p>
                 </div>
             </header>
 
             <main className="main">
                 <div className="container">
                     <h2 className="main__title">main</h2>
+                    <p className="main__description">Lorem ipsum dolor sit amet.</p>
                 </div>
             </main>
 
             <footer className="footer">
                 <div className="container">
                     <h2 className="footer__title">footer</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, veritatis?</p>
                 </div>
             </footer>
 
